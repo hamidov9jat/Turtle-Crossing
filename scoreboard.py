bbfcb87e0e5721bcdc30907e67fc3e954244f7b3
Nijat Hamidov <hamidov9jat@gmail.com>
2022-09-09 13:01:32 +0400

    refined scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -10,14 +10,18 @@ class Scoreboard(Turtle):
         self.penup()
         self.color('black')
         self._level = 1
+        self.goto(-290, 250)
         self.update_level()
 
     def update_level(self):
         self.clear()
-        self.goto(-290, 250)
         self.write(f"Level: {self._level}", font=FONT)
 
     def next_level(self):
         self._level += 1
         self.update_level()
+
+    def game_over(self):
+        self.home()
+        self.write(f"Game Over!", align='center', font=FONT)
 
